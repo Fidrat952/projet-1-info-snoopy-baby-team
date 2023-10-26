83be825060471480352f9ad14be6d886749a52eb
Fidrat95 <148880035+Fidrat95@users.noreply.github.com>
2023-10-26 15:02:04 +0200

    decortest ajouté

--- Décor.c
+++ Décor.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 void decor(){
-    char decor[10][20];
+    char decortest[10][20];
     for (int i=0;i<10;i++){
         printf("\n");
         for(int j=0;j<20;j++){
             decor[i][j] = '0';
-            printf("%c",decor[i][j]);
+            printf("%c",decortest[i][j]);
         }
     }
 }
